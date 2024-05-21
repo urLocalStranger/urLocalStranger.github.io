a1090c5989c426ebd702d5ea1a4d971e4c08e406
urLocalStranger <141691541+urLocalStranger@users.noreply.github.com>
2024-05-20 20:11:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -95,7 +95,9 @@ function perche(h, d){
   let z = [h,d];
   console.log(z);
   let angleR = 90 - atan(h / d);
+  console.log(angleR);
   let angleO = asin(1.33 * (sin(angleR)));
+  console.log(angleO);
   return (d * tan(90 - angleO)) - h;
 }
 
