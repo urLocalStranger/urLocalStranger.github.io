3e220c08249a8dfcf6221cf0fd4aeb60803593bc
urLocalStranger <141691541+urLocalStranger@users.noreply.github.com>
2024-05-21 13:46:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,434 @@
-eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('j 1U=["1l","1m","1n","1o","1V","1W","1X","1Y","14","1Z","20","21","22","23","25","26","27","28","29","2a","2b"];j 2c=["2d","2e","3e","2f","2g","2h","2i","1p","1q"]1r 3f=H.L("3g").2j("2k",2l);1r 3h=H.L("3i").2j("2k",2m);w 2l(){1U.1s(1t);D.E(1u(H.T));j U=q.I(1v(A("1l"),A("1m"),A("1n"),A("1o"))*C)/C;j 2n=q.I(2o(A("1V"),A("1W"),A("1X"),A("1Y"),U)*C)/C;j 2p=q.I(2q(A("14"),A("1Z"),A("21"),U)*C)/C;j 1w=2r(A("22"),A("23"),A("25"),A("26"),A("20"));j 2s=q.I(2t(A("27",V),A("28"),A("29"),A("2a"),A("2b"))*2u)/2u;H.L("2v").1x.1y="2w";G("2x","Ré15");G("a",`3j:${U}m`);G("b",`3k:${2n}m`);G("c",`3l:${2p}`);G("d",`3m:3n=${q.I(1w[0]*C)/C}N\\3o=${q.I(1w[1]*C)/C}N`);G("e",`3p:${2s}m`);G("2y","2z 2A 2B ré15!");H.L("3q").1x.1y="3r";2C.2D(0,0)}w 2m(){D.E("2");2c.1s(1t);D.E(1u(H.T));j U=q.I(1v(O("1l",V),O("1m",V),O("1n",V),O("1o",V))*C)/C;j 2E=q.I(2F(A("2d"),A("2e"))*C)/C;j 16=2G(A("2f"),A("2g"),A("2h"),A("2i"),A("1p"),O("14",V));D.E(16);j Z=2H(A("1q"),A("1p"),16[1],U,O("14",V));D.E(Z);j 2I=q.I(2J(Z[0],A("1q"),U,Z[1])*C)/C;H.L("2v").1x.1y="2w";G("2x","Ré15");G("a",`3s:${2E}m`);G("b",`3t:${q.I(16[0]*C)/C}°`);G("c",`3u:${q.I(Z[0]*C)/C}m`);G("d",`3v:${2I}s`);G("e",`3w:${q.I(Z[2]*C)/C}J`);G("2y","2z 2A 2B ré15!");2C.2D(0,0)}w 1v(x,a,v,b){j 2K=2L(v)*a;j 2M=17(b,-9.8,0,"3x");l 2K*(-2M/x)}w 2o(a,b,c,d,e){j 1z=2N(a,d,"3y");j 2O=1z-b;j 2P=1A(c,1z,d,"iôîo");l 1A(2O,2P,e,"î3zô")}w 2q(m,v,d,a){j 2Q=18(m,v,"2R");j 1B=1C(m,9.8,a/2,"3A");D.E("3B = "+1B);j 1D=2Q+1B;D.E("3C = "+1D);l 1E(1D,d,"3D")}w 2r(m,a,b,c,d){j 1F=9.8*m;j 2S=1F*19(d);j 2T=1F*P(d);j 2U=2S*a;j 1G=2T+2U;j 2V=1G*P(c);j 2W=2V/(P(b));l[2W,1G]}w 2t(a,b,c,d,h){j 1H=2X((b/C),(c/C),a);j 1I=0.3E*((1/1H[0])+(1/1H[1]));D.E("3F = "+1I);j 1J=1I+d;D.E("3G = "+1J);j f=1/1J;l((0.3/h)+1)*f}w 2F(h,d){j z=[h,d];D.E(z);j 1K=2Y-1L(h/d);D.E(1K);j 1M=1a((P(1K))/1.33);D.E(1M);l(d*1N(2Y-1M))-h}w 2G(a,b,c,d,e,m){j z=[a,b,c,d,e,m];D.E(z);j 2Z=1E(c,d,"3H");j 1O=q.M((a*9.8)/P(2*e));j 30=18(m,1O,"2R");j 31=30-2Z;j 32=1C(m,9.8,31,"3I");j 34=1a(32/b);l[34,1O]}w 2H(a,b,c,d,m){j z=[a,b,c,d,m];D.E(z);j 13=c*P(b);D.E("13 = "+13);j y=(d/2)-a;D.E("y = "+y);j t=17(y,-9.8,13,"3J");D.E("t = "+t);j 1b=c*19(b);D.E("1b = "+1b);j 1c=17(y,-9.8,13,"3K");D.E("1c = "+1c);j 1d=18(1c,m,"3L");D.E("1d = "+1d);l[1b*t,t,1d]}w 2J(a,b,c,d){j z=[a,b,c,d];D.E(z);35=q.M((a^2)+((b-(c/2))^2));l(35/3M)+d}w 3N(a){a.1s(36)}w 36(n){O(n)}w 1t(a){1r 1e=3O 3P();1e.3Q(1e.3R()+(7*24*37*37*3S));j 1f="1f="+1e.3T();j 10=H.L(a).10 H.T=a+"="+10+";"+1f+";38=/";H.T=a+"="+10+";"+1f+";38=/3U";D.E("3V 3W!")}w O(a,b){j T=1u(H.T);j 39=T.3X(/;/g,"");j c=39.3Y(/=|\\s/g);3Z(j i=0;i<=c.Q-2;i+=2){K(c[i]==a){K(40){H.L(a).10=c[i+1]}1P{l 3a(c[i+1])}}1P{41}}}w G(a,b){H.L(a).42=b}w A(a,b){1Qé1R=H.L(a).10;K(43){l 3a(1Qé1R)}1P{l 1Qé1R}}w 44(x){l x*3.6}w 2L(x){l x/3.6}w P(a){l q.P(a*(q.11/12))}w 19(a){l q.19(a*(q.11/12))}w 1N(a){l q.1N(a*(q.11/12))}w 1a(d){l q.1a(d)*(12/q.11)}w 3b(d){l q.3b(d)*(12/q.11)}w 1L(d){l q.1L(d)*(12/q.11)}w 2X(a,b,c){S(c.45().46()){u"47":l[a,b];u"48":l[-a,-b];u"mé3c 49":l[-(q.1S(a,b)),q.3d(a,b)];u"mé3c 4a":l[q.1S(a,b),-(q.3d(a,b))];W:l[0,0]}}w 17(b,c,e,p){j d=[b,c,e];K(p.Q!=5){l"X 1T!"}j x=d[p.B(\'x\')];j a=d[p.B(\'a\')];j i=d[p.B(\'i\')];j f=d[p.B(\'f\')];j t=d[p.B(\'t\')];S(p.Y(3,5)){u"4b":l i+(a*t);u"4c":l f-(a*t);u"4d":l(f-i)/t;u"4e":l(f-i)/a;u"4f":l((i+f)*t)/2;u"4g":l((2*x)/t)-f;u"4h":l((2*x)/t)-i;u"4i":l(2*x)/(i+f);u"4j":l q.M(q.F(i,2)+(2*a*x));u"4k":l q.M(q.F(f,2)-(2*a*x));u"4l":l(q.F(f,2)-q.F(i,2))/(2*x);u"4m":l(q.F(f,2)-q.F(i,2))/(2*a);u"4n":l(i*t)+(0.5*a*q.F(t,2));u"K":l(x-(0.5*a*q.F(t,2)))/t;u"4o":l(x-(i*t))/(0.5*q.F(t,2));u"4p":l q.1S(((-i+q.M(q.F(i,2)+(2*a*x)))/(a)),(((-i-q.M(q.F(i,2)+(2*a*x)))/(a))));u"4q":l(f*t)-(0.5*a*q.F(t,2));u"4r":l(x+(0.5*a*q.F(t,2)))/t;u"4s":l(x-(f*t))/(-0.5*q.F(t,2));u"4t":l(-f+q.M(q.F(f,2)-(2*a*x)))/(-a);W:l"...?"}}w 4u(a,b,p){j d=[a,b];K(p.Q!=3){l"X 1T!"}j f=d[p.B(\'f\')];j x=d[p.B(\'x\')];j k=d[p.B(\'k\')];S(p.Y(2,3)){u"f":l x*k;u"x":l f/k;u"k":l f/x;W:l"...?"}}w 1E(a,b,p){j d=[a,b];K(p.Q!=3){l"X 1T!"}j e=d[p.B(\'e\')];j x=d[p.B(\'x\')];j k=d[p.B(\'k\')];S(p.Y(2,3)){u"e":l 0.5*q.F(x,2)*k;u"k":l e/(q.F(x,2)*0.5);u"x":l q.M(e/(0,5*k))}}w 18(a,b,p){j d=[a,b];K(p.Q!=3){l"X 1g!"}j e=d[p.B(\'e\')];j m=d[p.B(\'m\')];j v=d[p.B(\'v\')];S(p.Y(2,3)){u"e":l 0.5*q.F(v,2)*m;u"m":l e/(q.F(v,2)*0.5);u"v":l q.M(e/(0,5*m));W:l"...?"}}w 1C(a,b,c,p){j d=[a,b,c];K(p.Q!=4){l"X 1g!"}j e=d[p.B(\'e\')];j m=d[p.B(\'m\')];j g=d[p.B(\'g\')];j h=d[p.B(\'h\')];S(p.Y(3,4)){u"e":l m*g*h;u"m":l e/(g*h);u"g":l e/(m*h);u"h":l e/(m*g);W:l"...?"}}w 2N(a,b,p){j d=[a,b];K(p.Q!=3){l"X 1g!"}j o=d[p.B(\'o\')];j i=d[p.B(\'i\')];j f=d[p.B(\'f\')];S(p.Y(2,3)){u\'f\':l 1/((1/o)+(1/i));u\'o\':l 1/((1/f)-(1/i));u\'i\':l 1/((1/f)-(1/o));W:l"...?"}}w 1A(a,b,c,p){j d=[a,b,c];K(p.Q!=4){l"X 1g!"}j 1h=d[p.B(\'o\')];j 1i=d[p.B(\'ô\')];j 1j=d[p.B(\'i\')];j 1k=d[p.B(\'î\')];S(p.Y(3,4)){u"o":l 1j/(-1k/1i);u"i":l 1h*(-1k/1i);u"ô":l-1k/(1j/1h);u"î":l-1i*(1j/1h);W:l"...?"}}',62,279,'|||||||||||||||||||let||return|||||Math||||case||function||||id|indexOf|100|console|log|pow|element|document|round||if|getElementById|sqrt||getCookie|sin|length||switch|cookie|hC|false|default|invalid|slice|dM|value|PI|180|viy|n3d1|sultats|aP|mrua|ek|cos|asin|vix|vf|ekTour|date|expires|params|ho|do_|hi|di|n1d1|n1d2|n1d3|n1d4|n7d5|n8d1|const|forEach|newCookie|decodeURIComponent|hChateau|tC|style|display|do2|grandissement|epf|ep|em|epr|fg|ft2|r12|vr1|vrt|angleR|atan|angleO|tan|viR|else|donn|ehtml|max|param|idArray1|n2d1|n2d2|n2d3|n2d4|n3d2|n3d3|n3d4|n4d1|n4d2||n4d3|n4d4|n5d1|n5d2|n5d3|n5d4|n5d5|idArray2|n6d1|n6d2|n7d1|n7d2|n7d3|n7d4|addEventListener|click|calculs1|calculs2|dC|dChateau|kR|kRessort|tCordes|dl|dLentille|10000|cdon|none|h1|intror|Et|voici|les|window|scrollTo|hP|perche|aPente|dMontagne|tS|tSon|ombreC|ms|longueurB|doif|di1|hi1|ekf|mve|fgy|fgx|ff|ft1x|ft1|rayons|90|eRessort|ekLancement|ekfPente|hPente||angle|dSon|mehCookie|60|path|cs|Number|acos|nisque|min|n6d3|start1|btn|start2|btnp2|no1|no2|no3|no4|t1|nt2|no5|next|flex|no6|no7|no8|no9|no10|taixf|fio|io|mghe|Ep|Em|exk|52|C1|Ctot|kxe|mgeh|xaitf|xaift|vme|340|setCookie|new|Date|setTime|getTime|1000|toUTCString|tester2|New|Cookie|replace|split|for|html|continue|innerText|nb|kmh|trim|toLowerCase|biconvexe|biconcave|convergeant|divergeant|fx|ix|ax|tx|xa|ia|fa|ta|ft|it|at|xt|xf|af|tf|xi|fi|ai|ti|fr'.split('|'),0,{}))
+let idArray1 = ["n1d1", "n1d2", "n1d3", "n1d4", "n2d1", "n2d2", "n2d3", "n2d4", "n3d1", "n3d2", "n3d3", "n3d4", "n4d1", "n4d2", "n4d3", "n4d4", "n5d1", "n5d2", "n5d3", "n5d4", "n5d5"];
+let idArray2 = ["n6d1", "n6d2", "n6d3", "n7d1", "n7d2", "n7d3", "n7d4", "n7d5", "n8d1"]
+const start1 = document.getElementById("btn").addEventListener("click", calculs1);
+const start2 = document.getElementById("btnp2").addEventListener("click", calculs2);
+
+function calculs1(){
+  idArray1.forEach(newCookie);
+  console.log(decodeURIComponent(document.cookie));
+  let hC = Math.round(hChateau(id("n1d1"), id("n1d2"), id("n1d3"), id("n1d4")) * 100)/100;
+  let dC = Math.round(dChateau(id("n2d1"), id("n2d2"), id("n2d3"), id("n2d4"), hC) * 100)/100;
+  let kR = Math.round(kRessort(id("n3d1"), id("n3d2"), id("n3d4"), hC) * 100)/100;
+  let tC = tCordes(id("n4d1"), id("n4d2"), id("n4d3"), id("n4d4"), id("n3d3"));
+  let dl = Math.round(dLentille(id("n5d1", false), id("n5d2"), id("n5d3"), id("n5d4"), id("n5d5")) * 10000)/10000;
+  
+  document.getElementById("cdon").style.display = "none";
+  element("h1", "Résultats");
+  element("a", `no1: ${hC} m`);
+  element("b", `no2: ${dC} m`);
+  element("c", `no3: ${kR}`);
+  element("d", `no4: t1 = ${Math.round(tC[0] * 100)/100} N\nt2 = ${Math.round(tC[1] * 100)/100} N`);
+  element("e", `no5: ${dl} m`);
+  element("intror", "Et voici les résultats!");
+  document.getElementById("next").style.display = "flex";
+  window.scrollTo(0,0);
+}
+
+function calculs2(){
+  console.log("2");
+  idArray2.forEach(newCookie);
+  console.log(decodeURIComponent(document.cookie));
+  let hC = Math.round(hChateau(getCookie("n1d1", false), getCookie("n1d2", false), getCookie("n1d3", false), getCookie("n1d4", false)) * 100)/100;
+  let hP = Math.round(perche(id("n6d1"), id("n6d2")) * 100)/100;
+  let aP = aPente(id("n7d1"), id("n7d2"), id("n7d3"), id("n7d4"), id("n7d5"), getCookie("n3d1", false), hC);
+  console.log(aP);
+  let dM = dMontagne(id("n8d1"), id("n7d5"), aP[1], hC);
+  console.log(dM);
+  let tS = Math.round(tSon(dM[0], id("n8d1"), hC, dM[1]) * 100)/100;
+  
+  document.getElementById("cdon").style.display = "none";
+  element("h1", "Résultats");
+  element("a", `no6: ${hP} m`);
+  element("b", `no7: ${Math.round(aP[0] * 100)/100} °`);
+  element("c", `no8: ${Math.round(dM[0] * 100)/100} m`);
+  element("d", `no9: ${tS} s`);
+  element("e", `no10: ${Math.round(aP[2] * 100)/100} J`);
+  element("intror", "Et voici les résultats!");
+  window.scrollTo(0,0);
+}
+
+function hChateau(x, t1, v, t2){
+  let ombreC = ms(v) * t1;
+  let longueurB = mrua(t2, -9.8, 0, "taixf");
+  return ombreC * (-longueurB/x);
+}
+ 
+function dChateau(f2, d12, hi2, di2, hChateau){
+  let do2 = doif(f2, di2, "fio");
+  let di1 = do2 - d12;
+  let hi1 = grandissement(hi2, do2, di2, "iôîo");
+  return grandissement(di1, hi1, hChateau, "îioô");
+}
+
+function kRessort(m, v, d, hChateau){
+  let ekf = ek(m, v, "mve");
+  let epf = ep(m, 9.8, hChateau/2, "mghe");
+  console.log("Ep = " + epf);
+  let em = ekf + epf; //Aussi epr
+  console.log("Em = " + em);
+  return epr(em, d, "exk");
+}
+
+function tCordes(m, coeff, angle1, angle2, anglePente){
+  let fg = 9.8 * m;
+  let fgy = fg * cos(anglePente);
+  let fgx = fg * sin(anglePente);
+  let ff = fgy * coeff;
+  let ft2 = fgx + ff;
+  let ft1x = ft2 * sin(angle2); //Ft1x = Ft2x
+  let ft1 = ft1x / (sin(angle1));
+  return [ft1, ft2];
+}
+
+function dLentille(forme, r1, r2, vr, h){
+  let r12 = rayons((r1/100), (r2/100), forme);
+  let vr1 = 0.52 * ((1/r12[0])+(1/r12[1]));
+  console.log("C1 = " + vr1);
+  let vrt = vr1 + vr;
+  console.log("Ctot = " + vrt);
+  let f = 1 / vrt;
+  return ((0.3 / h) + 1) * f;
+}
+
+function perche(h, d){
+  let z = [h,d];
+  console.log(z);
+  let angleR = 90 - atan(h / d);
+  console.log(angleR);
+  let angleO = asin((sin(angleR)) / 1.33);
+  console.log(angleO);
+  return (d * tan(90 - angleO)) - h;
+}
+
+function aPente(portee, lpente, kr, xressort, angleR, m, hChateau){
+  let z = [portee, lpente, kr, xressort, angleR, m];
+  console.log(z);
+  let eRessort = epr(kr, xressort, "kxe");
+  let viR = Math.sqrt((portee * 9.8) / sin(2 * angleR));
+  let ekLancement = ek(m, viR, "mve");
+  let ekfPente = ekLancement - eRessort;
+  let hPente = ep(m, 9.8, ekfPente, "mgeh");
+  let angle =asin(hPente / lpente);
+  
+  let ekTour = ekLancement - (ep(m, 9.8, hChateau/2, "mghe"));
+
+  return [angle, viR, ekTour];
+}
+
+function dMontagne(hMontagne, aCatapulte, viCatapulte, hChateau){
+  let viy = viCatapulte * sin(aCatapulte);
+  let y = (hChateau / 2) - hMontagne;
+  let t = mrua(y, -9.8, viy, "xaitf");
+  let vix = viCatapulte * cos(aCatapulte);
+  
+  return [vix * t, t];
+}
+
+function tSon(dMontagne, hMontagne, hChateau, temps){
+  dSon = Math.sqrt((dMontagne ^ 2) + ((hMontagne - (hChateau / 2)) ^ 2));
+  return (dSon / 340);
+}
+
+
+function setCookie(a){
+  a.forEach(mehCookie);
+}
+
+function mehCookie(n){
+  getCookie(n);
+}
+
+function newCookie(name){
+  const date = new Date();
+  date.setTime(date.getTime() + (7 * 24 * 60 * 60 * 1000));
+  let expires = "expires="+date.toUTCString();
+  let value = document.getElementById(name).value
+  document.cookie = name + "=" + value + ";" + expires + ";path=/";
+  document.cookie = name + "=" + value + ";" + expires + ";path=/tester2";
+  console.log("New Cookie!");
+}
+
+function getCookie(name, html = true){
+	let cookie = decodeURIComponent(document.cookie);
+  let cs = cookie.replace(/;/g, "");
+	let c = cs.split(/=|\s/g);
+  for(let i = 0; i <= c.length - 2; i+=2){
+    if (c[i] == name){
+      if(html){
+        document.getElementById(name).value = c[i+1];
+      }
+      else{
+        return Number(c[i+1]);
+      }
+    }
+    else{
+      continue;
+    }
+  }
+}
+
+function element(id, content){
+  document.getElementById(id).innerText = content;
+}
+
+function id(code, nb = true){
+  donnéehtml = document.getElementById(code).value;
+  if (nb){
+    return Number(donnéehtml);
+  }
+  else{
+    return donnéehtml;
+  }
+}
+
+//Fonctions pour les formules de physique...
+function kmh(x) {
+  return x * 3.6;
+}
+
+function ms(x) {
+  return x / 3.6;
+}
+
+function sin(angle){
+  return Math.sin(angle * (Math.PI / 180));
+}
+
+function cos(angle){
+  return Math.cos(angle * (Math.PI / 180));
+}
+
+function tan(angle){
+  return Math.tan(angle * (Math.PI / 180));
+}
+
+function asin(d){
+  return Math.asin(d) * (180 / Math.PI);
+}
+
+function acos(d){
+  return Math.acos(d) * (180 / Math.PI);
+}
+
+function atan(d){
+  return Math.atan(d) * (180 / Math.PI);
+}
+
+function rayons(r1, r2, forme){
+  switch (forme.trim().toLowerCase()){
+    case "biconvexe":
+      return [r1, r2];
+    case "biconcave":
+        return [-r1, -r2];
+    case "ménisque convergeant":
+      return [-(Math.max(r1, r2)),Math.min(r1, r2)];
+    case "ménisque divergeant":
+      return [Math.max(r1, r2),-(Math.min(r1, r2))];
+    default:
+      return [0,0];
+  }
+}
+
+function mrua(d1, d2, d3, p) {
+  //params: i=vi, f=vf, x=Δx, t=Δt, a=a
+  let d = [d1, d2, d3];
+
+  if(p.length != 5){
+    return "invalid param!";
+  }
+
+  let x = d[p.indexOf('x')];
+  let a = d[p.indexOf('a')];
+  let i = d[p.indexOf('i')];
+  let f = d[p.indexOf('f')];
+  let t = d[p.indexOf('t')];
+
+  switch (p.slice(3,5)) {
+    case "fx":
+      return i + (a * t);
+    case "ix":
+      return f - (a * t);
+    case "ax":
+      return (f - i) / t;
+    case "tx":
+      return (f - i) / a;
+    case "xa":
+      return ((i + f) * t) / 2;
+    case "ia":
+      return ((2 * x) / t) - f;
+    case "fa":
+      return ((2 * x) / t) - i;
+    case "ta":
+      return (2 * x) / (i + f);
+    case "ft":
+      return Math.sqrt(Math.pow(i,2) + (2 * a * x));
+    case "it":
+      return Math.sqrt(Math.pow(f,2) - (2 * a * x));
+    case "at":
+      return (Math.pow(f,2) - Math.pow(i,2)) / (2 * x);
+    case "xt":
+      return (Math.pow(f,2) - Math.pow(i,2)) / (2 * a);
+    case "xf":
+      return (i * t) + (0.5 * a * Math.pow(t,2));
+    case "if":
+      return (x - (0.5 * a * Math.pow(t,2))) / t;
+    case "af":
+      return (x - (i * t)) / (0.5 * Math.pow(t,2));
+    case "tf":
+        return Math.max(((-i + Math.sqrt(Math.pow(i,2) + (2 * a * x))) / (a)), (((-i - Math.sqrt(Math.pow(i,2) + (2 * a * x))) / (a))));
+    case "xi":
+      return (f * t) - (0.5 * a * Math.pow(t,2));
+    case "fi":
+      return (x + (0.5 * a * Math.pow(t,2))) / t;
+    case "ai":
+      return (x - (f * t)) / (-0.5 * Math.pow(t,2));
+    case "ti":
+      return (-f + Math.sqrt(Math.pow(f,2) - (2 * a * x))) / (-a);
+    default:
+      return "...?";
+  }
+}
+
+function fr(d1, d2, p){
+  //params: f=fRessort, x=Δx, k=kr
+  let d = [d1, d2];
+
+  if(p.length != 3){
+    return "invalid param!";
+  }
+
+  let f = d[p.indexOf('f')];
+  let x = d[p.indexOf('x')];
+  let k = d[p.indexOf('k')];
+
+  switch (p.slice(2,3)){
+    case "f":
+      return x * k;
+    case "x":
+      return f / k;
+    case "k":
+      return f / x;
+    default:
+      return "...?";
+  }
+}
+
+function epr(d1, d2, p){
+  //params: e=Epotentiel de rappel, k=kr, x=Δx
+  let d = [d1, d2];
+
+  if(p.length != 3){    
+    return "invalid param!";  
+  }  
+  let e = d[p.indexOf('e')];  
+  let x = d[p.indexOf('x')];  
+  let k = d[p.indexOf('k')];
+
+  switch(p.slice(2,3)){
+  case "e":
+    return 0.5 * Math.pow(x,2) * k;
+  case "k":
+    return e/(Math.pow(x,2) * 0.5);
+  case "x":
+    return Math.sqrt(e/(0,5 * k));
+
+  }
+}
+
+function ek(d1, d2, p){
+  //params: e=Ek,m=masse, v=vitesse
+  let d = [d1, d2];
+  if(p.length != 3){
+    return "invalid params!";
+  }
+  
+  let e = d[p.indexOf('e')];
+  let m = d[p.indexOf('m')];
+  let v = d[p.indexOf('v')];
+
+  switch(p.slice(2,3)){
+    case "e":
+      return 0.5 * Math.pow(v,2) * m;
+   case "m":
+      return e/(Math.pow(v,2) * 0.5);
+    case "v":
+      return Math.sqrt(e/(0,5 * m));
+    default:
+      return "...?";
+  }
+}
+
+function ep(d1, d2, d3, p){
+  //params: e=Ek, m=masse, g=gravité, h=hauteur
+  let d = [d1, d2, d3];
+  if(p.length != 4){
+    return "invalid params!";
+  }
+
+  let e = d[p.indexOf('e')];
+  let m = d[p.indexOf('m')];
+  let g = d[p.indexOf('g')];
+  let h = d[p.indexOf('h')];
+  switch(p.slice(3,4)){
+    case "e":
+      return m * g * h;
+   case "m":
+      return e / (g * h);
+    case "g":
+      return e / (m * h);
+    case "h":
+      return e / (m * g);
+    default:
+      return "...?";
+  }
+}
+
+function doif(d1, d2, p){
+    //params: o=Do, i=Di, f=foyer
+  let d = [d1, d2];
+  if(p.length != 3){
+    return "invalid params!";
+  }
+
+  let o = d[p.indexOf('o')];
+  let i = d[p.indexOf('i')];
+  let f = d[p.indexOf('f')];
+
+  switch(p.slice(2,3)){
+    case 'f':
+      return 1 / ((1 / o) + (1 / i));
+   case 'o':
+      return 1 / ((1 / f) - (1 / i));
+    case 'i':
+      return 1 / ((1 / f) - (1 / o));
+    default:
+      return "...?";
+  }
+}
+
+function grandissement(d1, d2, d3, p){
+  //params: o=Ho, ô=Do, i=Hi, î=Di
+  let d = [d1, d2, d3];  
+  if(p.length != 4){
+    return "invalid params!";
+  }
+  let ho = d[p.indexOf('o')];
+  let do_ = d[p.indexOf('ô')];
+  let hi = d[p.indexOf('i')];
+  let di = d[p.indexOf('î')];
+
+  switch(p.slice(3,4)){
+    case "o":
+      return hi / (-di / do_);
+   case "i":
+      return ho * (-di / do_);
+    case "ô":
+      return -di / (hi / ho);
+    case "î":
+      return -do_ * (hi / ho);
+    default:
+      return "...?";
+  }
+}
+
+
