907b2ab29ee29db7d9e82d0d9fe65a5e7e3762f3
urLocalStranger <141691541+urLocalStranger@users.noreply.github.com>
2024-05-28 22:27:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -193,10 +193,10 @@ function vPhysicien(vi1, x1, t1, vf2){
 function vLasso(d1, vm, vc, d2, t){
   let vM = ms(vm);
   let vC = ms(vc);
-  let dFin = ((-vC * t) / vM) + d1;
-  let tLancer = (-vM * (d2 - d1)) / vC;
+  let dFin = -(vC-vM) * t + d1;
+  let tLancer = -((d2-d1)/(vC-vM));
   let tLasso = t - tLancer;
-  let xLasso = dFin - d2;
+  let xLasso = (tLasso * vM) + d2;
   let z = [vM, vC, dFin, tLancer, tLasso];
   console.log(z);
   return xLasso / tLasso;
