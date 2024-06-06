d2273251935d52157af2f00b14eb3d3f2227d21a
urLocalStranger <141691541+urLocalStranger@users.noreply.github.com>
2024-06-05 23:01:13 -0400

    Update sc4.js

--- sc4.js
+++ sc4.js
@@ -1,5 +1,5 @@
 let btnarr = [col1, col2, col3, col4, col5, col6, col7];
-const col1L = listener(btnarr[0]);
+/*const col1L = listener(btnarr[0]);
 const col2L = listener(btnarr[1]);
 const col3L = listener(btnarr[2]);
 const col4L = listener(btnarr[3]);
@@ -13,7 +13,22 @@ function listener(id){
   colnum = Number(id.replaceAll("col",""));
   start(colnum);
 });
-}
+}*/
+
+const col1L = document.getElementById(btnarr[0]).addEventListener("click", function(){
+  start(Number(btnarr[0].replaceAll("col","")));});
+const col2L = document.getElementById(btnarr[1]).addEventListener("click", function(){
+  start(Number(btnarr[1].replaceAll("col","")));});
+const col3L = document.getElementById(btnarr[2]).addEventListener("click", function(){
+  start(Number(btnarr[2].replaceAll("col","")));});
+const col4L = document.getElementById(btnarr[3]).addEventListener("click", function(){
+  start(Number(btnarr[3].replaceAll("col","")));});
+const col5L = document.getElementById(btnarr[4]).addEventListener("click", function(){
+  start(Number(btnarr[4].replaceAll("col","")));});
+const col6L = document.getElementById(btnarr[5]).addEventListener("click", function(){
+  start(Number(btnarr[5].replaceAll("col","")));});
+const col7L = document.getElementById(btnarr[6]).addEventListener("click", function(){
+  start(Number(btnarr[6].replaceAll("col","")));});
 
 function start(cn){
     let id;
